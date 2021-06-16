bb67f8b83766487f03a5b0c82fff8bf209cec423
avanisrivastava <78412779+avanisrivastava@users.noreply.github.com>
2021-06-16 09:04:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+menu_list_array = ["Veg Margherita Pizza","double cheese margherita","farm fresh mushroom pizza","corn pizza","chicken pizza"//add more items
+        ];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;1<menu_list_array.lenght;i++){
+htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML=htmldata;
+
+
+//Complete the code
+
+
+
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>"
+for(var i=0;1<menu_list_array.lenght;i++){
+htmldata=htmldata+'<div class="card">'
++'<img src="pizza.png"/>'
++menu_list_array[i]+'</div>'
+}
+htmldata=htmldata+"</section>"
+document.getElementById("display_addedmenu").innerHTML=htmldata;
+//Complete the code
+
+}
+
+function add_top(){
+var item=document.getElementById("add_item") .value ;
+menu_list_array.push(item);
+add_item();}
